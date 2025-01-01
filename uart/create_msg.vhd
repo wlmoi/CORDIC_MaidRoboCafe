41f6ceda35dfe54b6fa94baaca4ddb8e700953f3
Rifki Afriadi <166176893+rifkiiiafdd@users.noreply.github.com>
2025-01-01 15:38:34 +0700

    modify uart top entity

--- uart/create_msg.vhd
+++ uart/create_msg.vhd
@@ -5,18 +5,20 @@ use ieee.numeric_std.all;
 entity create_msg is
     port (
         clk : in std_logic;
-        dT : in unsigned(17 downto 0); -- 101101010110101001 -53,425
+        dT : in unsigned(17 downto 0); -- 110000001110101001 -53,425
         i_start : IN STD_LOGIC;
         R : in unsigned(11 downto 0); -- 001011010111 363,5
         create_done : out std_logic := '0';
-        msg : out std_logic_vector(179 downto 0)
+        reset : in std_logic;
+        msg : out std_logic_vector(189 downto 0)
+
     );
 end create_msg;
 
 architecture behavioral of create_msg is
 
-    signal state : std_logic_vector(2 downto 0) := "000";
-    signal next_state : std_logic_vector(2 downto 0) := "000";
+    signal state : std_logic_vector(3 downto 0) := "1111";
+    signal next_state : std_logic_vector(3 downto 0) := "1111";
     signal convert_done : std_logic := '0';
     signal data_biner : unsigned(10 downto 0) := (others => '0');
     signal data_bcd : unsigned(15 downto 0) := (others => '0');
@@ -45,16 +47,28 @@ begin
     begin
         if rising_edge(clk) then
             state <= next_state;
-            i_RST <= '0';
+            -- i_rst <= '0';
             
         end if;
-
+    end process;
+    
+    process(state, dT, R, convert_done, i_start)
+    begin
         case state is
-            when "000" => -- pengisian dT:
+            when "1111" =>-- idle
+                if i_start = '1' then
+                    next_state <= "0000";
+                    create_done <= '0';
+                else 
+                    next_state <= "1111";
+                end if;
+
+            when "0000" => -- pengisian dT:
                 msg(29 downto 0) <= "100111010010101010001011001000";
-                next_state <= "001";
-            when "001" => -- pengisian tanda - dt
+                next_state <= "0001";
+            when "0001" => -- pengisian tanda - dt
                 i_rst <= '1';
+    
                 if dT(17) = '1' then
                     msg(39 downto 30) <= "1001011010";
                     
@@ -62,70 +76,80 @@ begin
                     msg(39 downto 30) <= "1001010110";
                 end if;    
 
-                next_state <= "010";
-            when "010" =>  -- pengisian angka di depan koma dT
-                -- data_biner <= "0000" & dT(16 downto 10);
-                data_biner <= "00000110101";
+                next_state <= "0010";
+
+            when "0010" =>  -- pengisian angka di depan koma dT
+                i_rst <= '0';
+                data_biner <= "000" & dT(16 downto 9);
+                -- data_biner <= "00010000001";
                 
-                msg(59 downto 40) <= "10011" & std_logic_vector(data_bcd(3 downto 0)) & '0' & "10011" & std_logic_vector(data_bcd(7 downto 4) & '0');
+                msg(69 downto 40) <= "10011" & std_logic_vector(data_bcd(3 downto 0)) & '0' 
+                                    & "10011" & std_logic_vector(data_bcd(7 downto 4) & '0') 
+                                    & "10011" & std_logic_vector(data_bcd(11 downto 8)) & '0';
                 if convert_done = '0' then
-                    next_state <= "010";
+                    next_state <= "0010";
                 else 
-                    next_state <= "011";
+                    next_state <= "0011";
                     i_rst <= '1';
+        
                 end if;
 
-            when "011" => -- pengisian angka di belakang koma dT
-
-                data_biner <= "0" & dT(9 downto 0);
-                msg(99 downto 60) <=  "10011" & std_logic_vector(data_bcd(11 downto 8) & '0')  & "10011" & std_logic_vector(data_bcd(7 downto 4)) & '0' & "10011" & std_logic_vector(data_bcd(3 downto 0) & '0')  & "1001011000"; 
+            when "0011" => -- pengisian angka di belakang koma dT
+                i_rst <= '0';
+                data_biner <= "00" & dT(8 downto 0);
+                msg(109 downto 70) <=  "10011" & std_logic_vector(data_bcd(3 downto 0) & '0')  
+                                        & "10011" & std_logic_vector(data_bcd(7 downto 4)) & '0' 
+                                        & "10011" & std_logic_vector(data_bcd(11 downto 8) & '0')  & "1001011000"; 
                 if convert_done = '0' then
-                    next_state <= "011";
+                    next_state <= "0011";
                 else 
-                    next_state <= "100";
+                    next_state <= "0100";
                     i_rst <= '1';
+        
+
                 end if;
 
-            when "100" => -- pengisian R:
-                msg(119 downto 100) <= "10011101001010100100";
-                next_state <= "101";
+            when "0100" => -- pengisian R:
+                msg(129 downto 110) <= "10011101001010100100";
+                next_state <= "0101";
             
-            when "101" => -- pengisian isi R
+            when "0101" => -- pengisian isi R
+                i_rst <= '0';
                 data_biner <= R(11 downto 1);
-                msg(159 downto 120) <= "10011" & std_logic_vector(data_bcd(3 downto 0) & '0') & 
+                msg(169 downto 130) <= "10011" & std_logic_vector(data_bcd(3 downto 0) & '0') & 
                 "10011" & std_logic_vector(data_bcd(7 downto 4) & '0') & 
                 "10011" & std_logic_vector(data_bcd(11 downto 8) & '0') & 
                 "10011" & std_logic_vector(data_bcd(15 downto 12) & '0') ;
 
                 if convert_done = '0' then
-                    next_state <= "101";
+                    next_state <= "0101";
                 else 
-                    next_state <= "110";
+                    next_state <= "0110";
                     i_rst <= '1';
+        
                 end if;
             
-            when "110" => -- pengisian koma seetelah R
-                msg(169 downto 160) <=   "1001011000";
+            when "0110" => -- pengisian koma seetelah R
+                msg(179 downto 170) <=   "1001011000";
                 
                 if R(0) = '1' then
-                    msg(179 downto 170) <= "1001101010";
+                    msg(189 downto 180) <= "1001101010";
                 else 
-                    msg(179 downto 170) <= "1001100000";
+                    msg(189 downto 180) <= "1001100000";
                 end if;
 
-                next_state <= "111";
+                next_state <= "0111";
             
-            when "111" => -- selesai
+            when "0111" => -- done
                 create_done <= '1';
-
-                if i_start = '1' then
-                    next_state <= "000";
-                else 
-                    next_state <= "111";
+                if reset = '1' then
+                    next_state <= "1111";
+                    msg <= (others => '0');
                 end if;
 
             when others =>
-                next_state <= "000";
+                next_state <= "1111";
+                create_done <= '0';
         end case;
     end process;
 
--- uart/uart.vhd
+++ uart/uart.vhd
@@ -1,126 +1,152 @@
 -- Spesifikasi
 -- Menerima dua inputm yaitu X0 dan Y0 dengan nilai maksimal masin masing 1000
 
--- 
+--
 
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity uart is
-	port	(
-				i_CLOCK		:	in std_logic							;
-				i_DATA		:	in std_logic_vector(7 downto 0)			;	----Apa yang akan dikirim, 8 bit atau 1 byte, berasal dari komponen lain
-				i_SEND		:	in std_logic							;	----Sinyal "kirim data", berasal dari komponen lain
-				i_DISPLAY	:	in	std_logic							;	----Sinyal "tampilkan data", berasal dari komponen lain
-				o_TX		:	out std_logic	:='1'					;	----Garis keluaran
-				
-				i_RX		:	in std_logic							;	----Garis masukan
-				i_log_ADDR	:	in std_logic_vector(7 downto 0)			;	----RX memiliki log 255 register yang terhubung ke DATA_OUT, pilih alamat di sini
-				
-				o_sig_CRRP_DATA		:	out std_logic					;	----Sinyal data rusak
-				o_sig_RX_BUSY		:	out std_logic					;	---	tanda jika RX sibuk atau dalam mode menerima.
-				o_sig_TX_BUSY		:	out std_logic					;	---	tanda jika TX sibuk atau dalam mode mengirim.
-				
-				o_DATA_OUT		:	out std_logic_vector(7 downto 0)	;	----Pesan dari RX, 8 bit atau 1 byte
-				hex1			: 	out std_logic_vector(6 downto 0)		--- Sinyal HEX (7 Segmen).
-			);
+	port (
+		i_CLOCK     : in std_logic;
+		i_DISPLAY   : in std_logic;  -- Sinyal "tampilkan data", berasal dari komponen lain
+		i_RX        : in std_logic;  -- Garis masukan
+		i_rst       : in std_logic;
+		o_TX        : out std_logic := '1';  -- Garis keluaran
+		i_calc      : in std_logic;
+		lampu 		: out std_logic_vector(3 downto 0) := "1111";
+		-- RX memiliki log 255 register yang terhubung ke DATA_OUT, pilih alamat di sin
+		o_sig_CRRP_DATA : out std_logic;  -- Sinyal data rusak
+		o_sig_RX_BUSY   : out std_logic;  -- tanda jika RX sibuk atau dalam mode menerima.
+		o_sig_TX_BUSY   : out std_logic;  -- tanda jika TX sibuk atau dalam mode mengirim.
+		led             : out std_logic_vector(3 downto 0);
+		hex1            : out std_logic_vector(6 downto 0)  -- Sinyal HEX (7 Segmen).
+	);
 end uart;
 
-
 architecture behavior of uart is
-	--- SINYAL
-	signal r_TX_DATA	:	std_logic_vector(7 downto 0) := (others => '1')	;	---	Register yang menyimpan pesan untuk dikirim
-	signal s_TX_START	:	std_logic := '0'								;	---	Sinyal yang disimpan untuk memulai transmisi
-	signal s_TX_BUSY	:	std_logic										;	---	Sinyal yang disimpan yang mengingatkan komponen utama bahwa sub komponennya "TX" sedang sibuk
+	-- SINYAL
+	signal r_TX_DATA    : std_logic_vector(189 downto 0) := (others => '1');  -- Register yang menyimpan pesan untuk dikirim
+	signal s_TX_START   : std_logic := '0';  -- Sinyal yang disimpan untuk memulai transmisi
+	signal s_TX_BUSY    : std_logic;  -- Sinyal yang disimpan yang mengingatkan komponen utama bahwa sub komponennya "TX" sedang sibuk
+	signal s_rx_data    : std_logic_vector(7 downto 0);  -- Data RX yang dibaca dari Buffer RX
+	signal s_hex        : std_logic_vector(6 downto 0);  -- Sinyal HEX (7 Segmen) dari Konverter ASCII-HEX.
+	signal s_ascii      : std_logic_vector(7 downto 0);  -- Data RX yang dibaca dari Buffer RX dan menjadi input Konverter ASCII-HEX.
+	signal tx_done      : std_logic := '0';
+	signal tx_run       : std_logic := '0';
+	signal s_button_counter : integer range 0 to 50000000 := 0;  -- penghitung untuk menunda penekanan tombol.
+	signal s_allow_press : std_logic := '0';  -- sinyal untuk mengizinkan tombol ditekan.
+	signal i_send       : std_logic := '1';
+	signal state        : std_logic_vector(2 downto 0) := "001";
+	signal X1           : std_logic_vector(9 downto 0) := (others => '0');
+	signal Y1           : std_logic_vector(9 downto 0) := (others => '0');
+	signal X0           : std_logic_vector(9 downto 0) := (others => '0');
+	signal Y0           : std_logic_vector(9 downto 0) := (others => '0');
+	signal cordic_done  : std_logic := '0';
+	signal dT           : unsigned(17 downto 0);
+	signal R            : unsigned(11 downto 0);
+	signal msg          : std_logic_vector(189 downto 0);
+	signal msg_ready 	: std_logic_vector(189 downto 0);
+	signal create_done  : std_logic := '0';
+	signal i_start_msg  : std_logic := '0';
+	signal rst_msg 	: std_logic := '0';
 
-	signal s_rx_data	:	std_logic_vector	(7 downto 0)				;	--- Data RX yang dibaca dari Buffer RX
-	signal s_hex		:	std_logic_vector	(6 downto 0)				;	--- Sinyal HEX (7 Segmen) dari Konverter ASCII-HEX.
-	signal s_ascii		:	std_logic_vector	(7 downto 0)				;	--- Data RX yang dibaca dari Buffer RX dan menjadi input Konverter ASCII-HEX.
-	
-	signal s_log_addr		:	std_logic_vector (7 downto 0)	:= (others => '0')	;
-	signal s_button_counter	:	integer range 0 to 50000000	:= 0					;	--- penghitung untuk menunda penekanan tombol.
-	signal s_allow_press	: std_logic	:= '0'										;	--- sinyal untuk mengizinkan tombol ditekan.
-	--- AKHIR SINYAL
-	
-	--- KOMPONEN
+	-- KOMPONENT
 	component uart_tx is
-	port(
-	
-		i_CLOCK	:	in std_logic							;
-		i_START	:	in std_logic							;
-		o_BUSY	:	out std_logic							;
-		i_DATA	:	in std_logic_vector(7 downto 0)	;
-		o_TX_LINE:	out std_logic	:= '1'
-	
-	);
+		port (
+			i_CLOCK : in std_logic;
+			i_START : in std_logic;
+			o_BUSY  : out std_logic;
+			tx_done : out std_logic;
+			i_DATA  : in std_logic_vector(189 downto 0);
+			o_TX_LINE : out std_logic := '1'
+		);
 	end component;
-	
+
 	component uart_rx is
-	port(
-
-		i_CLOCK			:	in std_logic								;
-		i_RX			:	in std_logic								;
-		o_DATA			:	out std_logic_vector(7 downto 0)			;
-		i_log_ADDR		:	in std_logic_vector( 7 downto 0 )			;
-		o_sig_CRRP_DATA:	out std_logic := '0'						;
-		o_BUSY			:	out std_logic
-
-	);
+		port (
+			i_CLOCK         : in std_logic;
+			i_rst           : in std_logic;
+			i_RX            : in std_logic;
+			o_sig_CRRP_DATA : out std_logic := '0';  -- Currupted data flag
+			x_biner         : out std_logic_vector(9 downto 0) := (others => '0');
+			y_biner         : out std_logic_vector(9 downto 0) := (others => '0');
+			o_BUSY          : out std_logic
+		);
 	end component;
-	
-	
+
 	component asciiHex is
-		port	(
-					i_ascii			:		in		std_logic_vector	(7 downto 0);
-					hex1			:		out		std_logic_vector	(6 downto 0)
-				);
+		port (
+			i_ascii : in std_logic_vector(7 downto 0);
+			hex1    : out std_logic_vector(6 downto 0)
+		);
 	end component;
-	--- AKHIR KOMPONEN
+
+	component cordic is
+		port (
+			x_in      : in std_logic_vector(9 downto 0);
+			y_in      : in std_logic_vector(9 downto 0);
+			cordic_on : in std_logic;
+			z         : out std_logic;
+			dT        : out unsigned(17 downto 0);
+			r_cordic  : out unsigned(11 downto 0);
+			clk       : in std_logic
+		);
+	end component;
+
+	component create_msg is
+		port (
+			clk         : in std_logic;
+			dT          : in unsigned(17 downto 0);  -- 1 10000001 110101001 -53,425
+			i_start     : in std_logic;
+			R           : in unsigned(11 downto 0);  -- 001011010111 363,5
+			create_done : out std_logic := '0';
+			reset       : in std_logic;
+			msg         : out std_logic_vector(189 downto 0)
+		);
+	end component;
+
 begin
-	
-	--- Modul Pengirim
-	u_TX	:	uart_tx port map(
-	
-		i_CLOCK	=>	i_CLOCK		,
-		i_START	=>	s_TX_START	,
-		o_BUSY	=>	s_TX_BUSY	,
-		i_DATA	=>	r_TX_DATA	,
-		
-		o_TX_LINE	=>	o_TX
-	
+	lampu(3) <= create_done;
+	lampu(2 downto 0) <= state;
+
+	led <= "0000";
+
+	-- Modul Penerima
+	u_RX : uart_rx port map (
+		i_CLOCK        => i_CLOCK,
+		i_rst          => i_rst,
+		i_RX           => i_RX,
+		o_sig_CRRP_DATA => o_sig_CRRP_DATA,
+		o_BUSY         => o_sig_RX_BUSY,
+		x_biner        => X1,
+		y_biner        => Y1
 	);
-	
-	--- Modul Penerima
-	u_RX	:	uart_rx port map(
-	
-		i_CLOCK				=>	i_CLOCK				,
-		i_RX				=>	i_RX				,
-		o_DATA				=>	s_rx_data			,
-		i_log_ADDR			=>	s_log_addr			,
-		o_sig_CRRP_DATA		=>	o_sig_CRRP_DATA		,
-		o_BUSY				=>	o_sig_RX_BUSY
-		
+
+	u_TX : uart_tx port map (
+		i_CLOCK  => i_CLOCK,
+		i_START  => s_TX_START,
+		o_BUSY   => s_TX_BUSY,
+		i_DATA   => r_TX_DATA,
+		tx_done  => tx_done,
+		o_TX_LINE => o_TX
 	);
-	
-	
-	--- Modul Konverter ASCII ke HEX (7 Segmen)
-	a2h	:	asciiHex	port map(
-		
-		i_ascii	=>	s_ascii	,
-		hex1		=>	s_hex
+
+	u_convert_msg : create_msg port map (
+		clk         => i_CLOCK,
+		dT          => dT,
+		i_start     => i_start_msg,
+		R           => R,
+		reset       => rst_msg,
+		create_done => create_done,
+		msg         => msg
 	);
-		
-	s_ascii <= s_rx_data;		---	data rx yang dibaca dari buffer menjadi input konverter ascii-hex
-	o_DATA_OUT <= s_rx_data;	--- data rx yang dibaca dari buffer ditampilkan pada LED[7 .. 0]
-	hex1	<= s_hex;			--- data rx yang dibaca dari buffer ditampilkan pada 7 Segmen.
-	
-	
-	------------------------------------------------------------
-	--- penundaan untuk menangani bouncing tombol
-	p_button		:	process(i_CLOCK) begin
-		if(rising_edge(i_CLOCK)) then
-			if(s_button_counter = 49999900) then
+
+	p_button : process(i_CLOCK)
+	begin
+		if rising_edge(i_CLOCK) then
+			if s_button_counter = 49999900 then
 				s_button_counter <= 0;
 				s_allow_press <= '1';
 			else
@@ -129,24 +155,28 @@ begin
 			end if;
 		end if;
 	end process;
-	--- akhir dari proses penundaan
 
-	--- Sub-rutin transmisi memeriksa ketersediaan untuk mengirim sesuatu sebelum mengirimnya,
-	---semua hal lain ditangani secara internal.
-	--- proses: untuk menampilkan data di Buffer RX. alamat buffer dipilih menggunakan sakelar. perintah display menggunakan tombol key_1.
-	p_DISPLAY_RX	:	process	(i_CLOCK) begin
-		if(rising_edge(i_CLOCK)) then
-			if(
-				i_DISPLAY = '0' and				--- Tombol Display ditekan
-				s_allow_press = '1'				--- tombol diizinkan untuk ditekan
-				) then
-				s_log_addr <= i_DATA;			---	setel alamat data yang ingin ditampilkan dari buffer RX. 
-			end if;
-		end if;
-	end process;
-	--- akhir dari proses
+	-- u_cordic : cordic port map(
+	--  x_in => X1,
+	--  y_in => Y1,
+	--  clk => i_CLOCK,
+	--  cordic_on => i_calc,
+	--  z => cordic_done,
+	--  dT => dT,
+	--  r_cordic => R
+	-- );
+
+	-- Modul Konverter ASCII ke HEX (7 Segmen)
+	a2h : asciiHex port map (
+		i_ascii => x1(7 downto 0),
+		hex1    => s_hex
+	);
+
+	s_ascii <= s_rx_data;  -- data rx yang dibaca dari buffer menjadi input konverter ascii-hex
+	hex1 <= s_hex;  -- data rx yang dibaca dari buffer ditampilkan pada 7 Segmen.
 	
-	--- proses: untuk melakukan pengiriman data. input data diatur dari sakelar. perintah pengiriman menggunakan tombol key_0.
+			
+
 	p_TRANSMIT	:	process(i_CLOCK) begin
 	
 		if(rising_edge(i_CLOCK)) then
@@ -154,12 +184,13 @@ begin
 		
 			--- Jika memungkinkan, kirim byte data di input.
 			if( 
-				i_SEND = '0' and 		----	Tombol Kirim ditekan
+				i_send = '0' and 		----	Tombol Kirim ditekan
 				s_TX_BUSY = '0' and 	----	pengirim tidak sibuk / tidak mengirim
 				s_allow_press = '1'		----  	tombol diizinkan untuk ditekan
 				) then 					----	Kirim pesan jika subkomponen "TX" tidak sibuk
 			
-				r_TX_DATA	<=	i_DATA;									----Berikan pesan subkomponen
+				-- r_TX_DATA	<=	"1001101010100101100010011001101001101100100110011010011000001001110100101010010010011010101001100100100110100010010110001001110010100110010010011000101001011010100111010010101010001011001000";									----Berikan pesan subkomponen
+				r_TX_DATA	<=	msg_ready;									----Berikan pesan subkomponen
 				s_TX_START	<=	'1';									----Beri tahu untuk mengirim
 
 			else
@@ -167,14 +198,67 @@ begin
 				s_TX_START <= '0';									----Jika Subkomponen "TX" sibuk, atau tombol tidak ditekan, jangan kirim
 				
 			end if;	---KEY(0) = '0' dan s_TX_BUSY = '0'
-			
-			------------------------------------------------------------
-			
-		
-		end if;	---rising_edge(i_CLOCK)
-	
+		end if;
 	end process;
-	
-	------------------------------------------------------------
 
+	process (state, i_calc, i_clock, create_done, tx_done)
+	begin
+		if rising_edge(i_clock) then
+			case state is
+				when "001" =>  -- idle
+					i_start_msg <= '0';
+					i_send <= '1';
+					-- rst_msg <= '1';
+					if (i_calc = '0') then
+						-- state <= "010";
+						state <= "100";
+						-- rst_msg <= '0';
+						
+					end if;
+
+				-- when "010" =>
+				-- 	start_calculation <= '1';
+
+				-- 	if cordic_done = '1' then
+				-- 		state <= "011";
+				-- 	else
+				-- 		state <= "010";
+				-- 	end if;
+
+				-- when "011" =>
+				-- 	calc_dt_start <= '1';
+
+				-- 	if calc_dt_done = '1' then
+				-- 		state <= "100";
+				-- 	else
+				-- 		state <= "011";
+				-- 	end if;
+
+				when "100" =>
+					
+					dT <= "110000001110101001";
+					R  <= "001011010111";
+					i_start_msg <= '1';
+					if create_done = '1' then
+						state <= "101";
+						i_start_msg <= '0';
+						msg_ready <= msg;
+					else
+						state <= "100";
+					end if;
+
+				when "101" =>
+					i_send <= '0';
+					rst_msg <= '1';
+
+					if tx_done = '1' then
+						i_send <= '1';
+						state <= "001";
+						rst_msg <= '0';
+					end if;
+
+				when others =>
+			end case;
+		end if;
+	end process;
 end behavior;
